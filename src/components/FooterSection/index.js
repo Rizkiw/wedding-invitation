d38b633b74bf8c77d6de9171cdedca159233c142
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2024-08-05 00:52:49 +0700

    update footer

--- src/components/FooterSection/index.js
+++ src/components/FooterSection/index.js
@@ -1,53 +1,30 @@
 import React, { Fragment } from 'react';
-import { bool } from 'prop-types';
-import { styWrapper } from '../HelloSection/styles';
 
-function FooterSection({ isInvitation }) {
+function FooterSection() {
   return (
     <Fragment>
-      {!isInvitation && (
-        <div id="fh5co-couple" className="fh5co-section-gray" css={styWrapper}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                <h2 className="main-font">Terima Kasih</h2>
-                <p className="info">
-                  Berdasarkan kondisi saat ini, tanpa mengurangi rasa hormat, diharapkan tetap bisa menjalin silaturahmi
-                  melalui media online, bukan dengan kontak fisik secara langsung. <br />
-                  Mohon pengertiannya.
-                </p>
-                <p className="info">
-                  Atas segala do'a dan restu saudara/i,
-                  <br /> kami ucapkan terima kasih. <br />
-                  Wassalamualaikum warahmatullahi wabarakatuh.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
       <footer id="fh5co-footer" role="contentinfo">
         <div className="container">
           <div className="row copyright">
             <div className="col-md-12 text-center">
               <p>
-                <small className="block">&copy; 2020 Dinda & Indra Wedding. All Rights Reserved.</small>
-                <small className="block">
+                <small className="block">Made with ❤️ by Rizki & Linda. <br />All Rights Reserved &copy; 2024.</small>
+                {/* <small className="block">
                   Covid-19 Icon by{' '}
-                  <a href="https://www.flaticon.com/packs/covid-protection-measures-5" target="_blank" rel="noreferrer">
+                  <a href="https://www.flaticon.com" target="_blank" rel="noreferrer">
                     Flat Icon - Frepik
                   </a>
-                </small>
+                </small> */}
                 <small className="block">
                   Song by{' '}
-                  <a href="https://www.youtube.com/watch?v=fb167KAxvrg" target="_blank" rel="noreferrer">
-                    Anandito Anisa - Pernikahan Impian
+                  <a href="https://www.youtube.com/watch?v=WNw2yH_waVc" target="_blank" rel="noreferrer">
+                    Juicy Luicy - Lagu Nikah
                   </a>
                 </small>
                 <small className="block">
                   Original Template from{' '}
-                  <a href="http://freehtml5.co/" target="_blank" rel="noreferrer">
-                    FREEHTML5.co
+                  <a href="https://indrakusuma.web.id/" target="_blank" rel="noreferrer">
+                    Indra Kusuma
                   </a>
                 </small>
               </p>
@@ -58,9 +35,5 @@ function FooterSection({ isInvitation }) {
     </Fragment>
   );
 }
-
-FooterSection.propTypes = {
-  isInvitation: bool.isRequired,
-};
 
 export default React.memo(FooterSection);
