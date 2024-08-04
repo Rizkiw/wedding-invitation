ebac5416a7e7d490fd5f8a87ff7e8991e1631639
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2024-08-04 19:42:47 +0700

    Update index.js

--- src/components/GenerateLink/index.js
+++ src/components/GenerateLink/index.js
@@ -154,7 +154,7 @@ function GenerateLink() {
   return (
     <div>
       <h2 className="title">Generator of Link Invitation</h2>
-      <h3 className="title__sub">Dinda & Indra Wedding</h3>
+      <h3 className="title__sub">Rizki & Linda Wedding</h3>
 
       {loading && <h4 style={{ textAlign: 'center' }}>Memuat data..</h4>}
 
