8e42ea4d45424b55d099950d3d25ff9c8dd6d48a
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2024-08-04 18:37:36 +0700

    Update isMobileDevice.js

--- src/helpers/isMobileDevice.js
+++ src/helpers/isMobileDevice.js
@@ -1,5 +1,5 @@
 function isMobileDevice() {
-  if (typeof window === undefined) return false;
+  // if (typeof window === undefined) return false;
 
   const userAgent = window.navigator.userAgent || '';
   const finalUserAgent = userAgent.toLocaleLowerCase();
@@ -7,7 +7,7 @@ function isMobileDevice() {
   if (finalUserAgent.includes('android')) return true;
   if (finalUserAgent.includes('ios')) return true;
   if (finalUserAgent.includes('iphone')) return true;
-  // if (finalUserAgent.includes('windows phone')) return true;
+  if (finalUserAgent.includes('windows phone')) return true;
   if (finalUserAgent.includes('bb')) return true;
 
   return false;
