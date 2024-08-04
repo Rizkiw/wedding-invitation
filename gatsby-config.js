d8337467ebb57f45697cc2f203dc895e2c0d5b2d
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2024-08-04 15:27:05 +0700

    modify config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: `/thekusuma`,
+  pathPrefix: `/rizki`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-eslint',
