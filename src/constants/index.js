064e216435a6b97d5762490a5f16d4bcd38594a1
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2024-08-04 15:27:37 +0700

    change wedding date

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,6 +7,6 @@ export const GOOGLE_MAPS_LINK = `https://g.page/HARRISHotelSentralandSemarang?sh
 /**
  * Wedding time
  */
-export const EPOCH_START_EVENT = 1613952139;
-export const EPOCH_END_EVENT = 1613952139;
-export const UTC_WEDDING_TIME = '2020-10-03:04:00:00Z';
+export const EPOCH_START_EVENT = 1764428400;
+export const EPOCH_END_EVENT = 1764435600;
+export const UTC_WEDDING_TIME = '2025-11-29:04:00:00Z';
