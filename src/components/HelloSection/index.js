1710a703357f3c9f9b165fdbe2c74de3fef7ddf7
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2025-03-14 00:29:41 +0700

    fix bug

--- src/components/HelloSection/index.js
+++ src/components/HelloSection/index.js
@@ -4,7 +4,6 @@ import 'aos/dist/aos.css';
 
 import Bride from '@assets/images/linda.jpg';
 import Groom from '@assets/images/rizki.jpg';
-import Bird from '@assets/images/burung/01.png';
 
 import { styWrapper } from './styles';
 
